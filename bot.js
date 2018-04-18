b77260b345f6cf0f8b7b8df90968df55b812f3f3
wwxnff <harrywangshc@gmail.com>
2018-04-18 16:45:36 -0400

    update

--- bot.js
+++ bot.js
@@ -5,6 +5,11 @@ var search = require('youtube-search');
 var isChinese = require('is-Chinese');
 var bodyparser = require('body-parser');
 var jsonparse = require('json-parse');
+var fs = require('fs');
+var ytdl = require('ytdl-core');
+var mongoClient = require('mongodb').Client();
+var connectionString = "mongodb+srv://admin:admin@bambooalbum-zokjp.mongodb.net/test";
+
 logger.remove(logger.transports.Console);
 logger.add(logger.transports.Console, {
     colorize: true
@@ -18,21 +23,34 @@ bot.login(auth.token);
 
 
 function playMusic(args){
-var search_opts = {
+	MongoClient.connect(connectionString, function(err,db){
+		var dbo = db.db("playlist");
+
+	var search_opts = {
 	maxResults: 1,
-	key: 'AIzaSyDMZFukDb8l8UaVa8EtQqUKm22vjzPGItU' // assume chinese song
+	key: 'AIzaSyDMZFukDb8l8UaVa8EtQqUKm22vjzPGItU' 
 	};
+	
 	var search_query = args.slice(0);
 	search_query.splice(0,1);
-	
 	if (isChinese(args[1])){
 		search_query = args[1];
 	}else{
 		search_query = search_query.toString();
 	}
-
-	search(search_query,search_opts,function(err,result){
+	dbo.collection("pl1").findOne({musicName: search_query},function(err,result){
+		if (result === null){
+		search(search_query,search_opts,function(err,result){
+		if (err) logger.error('Error while searching');
+		else logger.info("found the video");
 		var link = result[0].link;
+		ytdl(link).pipe(fs.createWriteStream('/music/'+ args + '.flv'));
+	});
+
+	}else{
+
+	}	
+	});
 	});
 }
 
@@ -49,11 +67,9 @@ bot.on('message', function(message) {
 		//console.log(message.member);
 		//message.member.voiceChannel.join().then(connection => console.log('Connected!'))
   		//								.catch(console.error);;
-
 		switch(cmd){
 			case 'test':
 				message.channel.send('This is a test');
-
 				//console.log(message.channel.guild);
 				break;
 			case 'play':
