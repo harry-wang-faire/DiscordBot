75fb7f56d9f901282424c743fdac16f1a039c19e
wwxnff <harrywangshc@gmail.com>
2018-04-20 11:34:23 -0400

    update

--- bot.js
+++ bot.js
@@ -20,14 +20,15 @@ logger.add(logger.transports.Console, {
 logger.add(logger.transports.File, { filename: 'discordLogs.log' });
 logger.level = 'debug';
 
-
+var playList = [];
+var pl = [];
 var search_opts = {
 	maxResults: 1,
 	key: 'AIzaSyDMZFukDb8l8UaVa8EtQqUKm22vjzPGItU' 
 	};
 
-function playMusic(args,message){
 
+function getQuery(args){
 	var search_query = args.slice(0);
 	search_query.splice(0,1);
 	if (isChinese(args[1])){
@@ -35,9 +36,16 @@ function playMusic(args,message){
 	}else{
 		search_query = search_query.join(' ');
 	}
+	return search_query;
+}
+
+function playMusic(args,message){
+	if (message.member.voiceChannel !== undefined){
+		var search_query = getQuery(args);
 		search(search_query,search_opts,function(err,result){
-		if (err) logger.error('Error while searching');
-		else logger.info("found the video");
+		if (err) logger.error('Error while searching and the error is ' + err);
+		else {
+		logger.info("found the video");
 		var link = result[0].link;
 		message.member.voiceChannel.join()
 			.then(function(connection){
@@ -46,8 +54,11 @@ function playMusic(args,message){
 				 logger.info("Playing");
 				})
 			.catch(console.error);
+	}
 	});
-
+}else{
+	message.channel.send("You are not in any Voice channel!");
+}
 }
  var dispatcher = null;
 
@@ -56,6 +67,9 @@ bot.on('ready',function(evt){
 	logger.info('Logged in as: ' + bot.user.username);
 });
 
+dispatcher.on('end',function(){
+
+});
 bot.on('message', function(message) {
 	//console.log(message.content);
 	if(message.content.substring(0,1) === '$'){
@@ -75,8 +89,12 @@ bot.on('message', function(message) {
 			.catch(console.error);
 				break;
 			case 'play':
+			if (args[1] !== "playlist")
 				playMusic(args,message);
-				break;
+			else {
+				playMusic(pl[0],message);
+			}
+			break;
 			case 'pause':
 				dispatcher.pause();
 				logger.info("Paused");
@@ -97,8 +115,40 @@ bot.on('message', function(message) {
 				break;
 			case 'help':
 				message.reply('play -music to play a music');
+				break;
 			case 'loop': 
 				message.channel.send('$loop');
+				break;
+			case 'add':
+				var query = getQuery(args);
+				playList.push(query);
+				pl.push(args);
+				logger.log(pl);
+				logger.log(playList);
+				message.reply("You have added "+query+" to your playList");
+				break;
+			case 'skip':
+				if (playList.length===0) message.reply("This is the last song!");
+				else if (dispatcher === null) message.reply("You are not playing any songs yet!");
+				else {
+					playMusic(pl[0],message);
+					pl.splice(0,1);
+					playlist.splice(0,1);
+					logger.info("Skipped a song");
+					playMusic(pl[0],message);
+				}
+				break;
+			case 'playlist':
+				if (playList.length===0) {
+				message.reply("Sorry, there are no songs in the playlist right now, use add to add -songName to add some")
+				}else{
+				message.channel.send("here is the playlist");
+				for (var i = 0; i < playList.length; i++){
+				message.channel.send(i+1 + " " + playList[i]);
+				}
+			}
+				break;
+
 
 		}
 	}
